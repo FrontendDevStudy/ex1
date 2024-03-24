14ea7b5007f8569d118bfdbf86cd0e9f93d6b876
OlenaOP <elena.peredriy@gmail.com>
2024-03-24 17:32:41 +0200

    add no scroll

--- mobile-menu-copy/script.js
+++ mobile-menu-copy/script.js
@@ -2,9 +2,14 @@
   const refs = {
     openMenuBtn: document.querySelector("[data-menu-open]"),
     closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menuLink: document.querySelectorAll(".mobile-menu a"),
     menu: document.querySelector("[data-menu]"),
     body: document.querySelector("body"),
   };
+
+  refs.menuLink.forEach(function (el) {
+    el.onclick = toggleMenu;
+  });
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
   refs.closeMenuBtn.addEventListener("click", toggleMenu);
